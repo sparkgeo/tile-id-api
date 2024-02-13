999cd270a74f589a041ace2e78976f342486da64
Tom Christian <tom@sparkgeo.com>
2024-02-13 13:54:55 -0800

    gofmt in pre-commit

--- scripts/dev-init.sh
+++ scripts/dev-init.sh
@@ -6,4 +6,8 @@ pushd $(dirname $0)/..
 
 pip install pre-commit
 pre-commit install
+
+curl --fail -o .pre-commit-gofmt.sh https://raw.githubusercontent.com/golang/go/release-branch.go1.1/misc/git/pre-commit
+chmod 755 .pre-commit-gofmt.sh
+
 pip install -r scripts/docker/requirements.txt
